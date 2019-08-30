d5e8f72a2bc9c382bffe250a2805ba765d2a405b
Sergey Masuryan <s.masuryan@visionlabs.ru>
2019-08-30 11:43:53 +0300

    test_no_30

--- run/build.sh
+++ run/build.sh
@@ -8,10 +8,10 @@ export WRP_ROOT_ARG=$1
 cmake .. \
  -DWRP_ROOT=$WRP_ROOT_ARG \
  -DCMAKE_INSTALL_PREFIX=../install \
- -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+ -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+ -DCODE_COVERAGE=ON
 
 
-cmake --build . --config $BUILD_TYPE -- -j24
+cmake --build . --config $BUILD_TYPE -- -j12
 cmake --build . --target install  
-
 
