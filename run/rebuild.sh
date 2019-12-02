0432aa1da1bd71c7fc17c6947004da506d72fc7d
Sergey Masuryan <s.masuryan@visionlabs.ru>
2019-12-02 11:48:18 +0300

    test:build with `-fsanitize`

--- run/rebuild.sh
+++ run/rebuild.sh
@@ -12,7 +12,13 @@ cmake .. \
  -DWRP_ROOT=$WRP_ROOT_ARG \
  -DCMAKE_INSTALL_PREFIX=../install \
  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
- -DCODE_COVERAGE=ON
+ -DCODE_COVERAGE=ON \
+ -DCMAKE_CXX_FLAGS_DEBUG="-g -O0 -Wpedantic -Wall -Wextra -Wnarrowing -pedantic -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option" \
+ -DCMAKE_CXX_FLAGS="-fsanitize=address  -fsanitize=leak -g" \
+ -DCMAKE_C_FLAGS="-fsanitize=address  -fsanitize=leak -g" \
+ -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address  -fsanitize=leak" \
+ -DCMAKE_MODULE_LINKER_FLAGS="-fsanitize=address  -fsanitize=leak"
+
 
 
 cmake --build . --config $BUILD_TYPE -- -j$3
