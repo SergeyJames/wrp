086dfbebfc43ccdf6d49b560c3bfb6be41b326c0
Sergey Masuryan <s.masuryan@visionlabs.ru>
2020-02-18 16:36:50 +0300

    new function, new test

--- run/build.sh
+++ run/build.sh
@@ -2,6 +2,4 @@ cd $1/build
 export BUILD_TYPE=$2
 export WRP_ROOT_ARG=$1
 
-cmake --build . --config $BUILD_TYPE -- -j$3
-cmake --build . --target install  
-
+cmake --build . --config $BUILD_TYPE --target install -- -j$3
--- test/test_numeric.cpp
+++ test/test_numeric.cpp
@@ -7,12 +7,21 @@ void test_int() {
 }
 
 void test_float() {
-	std::vector<float> dv{ 0.01f, 0.03f, 0.6f, 0.058f, 0.87f, 1.2f, 0.123f, 0.89f, 0.00111f };
-	ASSERT_NEAR(wrp::average(dv), 0.420f, 0.001f);
+	std::vector<float> fv{ 0.01f, 0.03f, 0.6f, 0.058f, 0.87f, 1.2f, 0.123f, 0.89f, 0.00111f };
+	ASSERT_NEAR(wrp::average(fv), 0.420f, 0.001f);
+	
+	std::vector<float> fvClone {std::begin(fv), std::begin(fv) + fv.size() - 1 };
+	fv.pop_back();
+	ASSERT_NEAR(wrp::average(std::begin(fv), std::end(fv)), wrp::average(fvClone), 0.001f);
 }
 
-TEST(Numeric, average)
-{
+void test_empty() {
+	std::vector<float> fv;
+	ASSERT_EQ(wrp::average(fv), 0.0f);
+}
+
+TEST(Numeric, average) {
 	test_int();
 	test_float();
+	test_empty();
 }
